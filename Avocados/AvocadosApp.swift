7ee8ccda108861df07b20a12b283790337f804c5
studioOwol <from.owol@gmail.com>
2023-05-09 14:50:08 +0900

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Nahyun on 2023/05/09.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
