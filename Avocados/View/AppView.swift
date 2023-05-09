2c92c572bde852875d8f445e482ac176c3429eb2
studioOwol <from.owol@gmail.com>
2023-05-09 17:32:10 +0900

    Tab View

---
+++ Avocados/View/AppView.swift
@@ -0,0 +1,44 @@
+//
+//  AppView.swift
+//  Avocados
+//
+//  Created by Nahyun on 2023/05/09.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        TabView {
+            AvocadosView()
+                .tabItem({
+                    Image("tabicon-branch")
+                    Text("Avocados")
+                })
+            ContentView()
+                .tabItem({
+                    Image("tabicon-book")
+                    Text("Recipes")
+                    
+                })
+            RipeningStagesView()
+                .tabItem({
+                    Image("tabicon-avocado")
+                    Text("Ripening")
+                })
+            SettingsView()
+                .tabItem({
+                    Image("tabicon-settings")
+                    Text("Settings")
+                })
+        }
+        .edgesIgnoringSafeArea(.top)
+        .accentColor(Color.primary)
+    }
+}
+
+struct AppView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppView()
+    }
+}
---
+++ Avocados/View/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  Avocados
+//
+//  Created by Nahyun on 2023/05/09.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
