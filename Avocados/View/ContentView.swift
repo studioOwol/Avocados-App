cde044f269a2bce578b7a7fac2093068c47009ac
studioOwol <from.owol@gmail.com>
2023-05-09 20:11:50 +0900

    Creat adaptive layouts design

--- Avocados/View/ContentView.swift
+++ Avocados/View/ContentView.swift
@@ -26,18 +26,26 @@ struct ContentView: View {
                     }
                 }
                 
+                //MARK: - Dishes
+                Text("Avocado Dishes")
+                    .fontWeight(.bold)
+                    .modifier(TitleModifier())
+                
+                DishesView()
+//                    .frame(maxWidth: 640)
+                
                 //MARK: - Footer
                 
                 VStack(alignment: .center, spacing: 20) {
                     Text("All About Avocados")
-                        .font(.system(.title, design: .serif))
                         .fontWeight(.bold)
-                        .foregroundColor(Color("ColorGreenAdaptive"))
-                    .padding(8)
+                        .modifier(TitleModifier())
+                        
                     Text("Everything you wanted to know about avocados but were too afraid to ask.")
                         .font(.system(.body, design: .serif))
                         .multilineTextAlignment(.center)
                         .foregroundColor(Color.gray)
+                        .frame(minHeight: 60)
                 }
                 .frame(maxWidth: 640)
                 .padding()
@@ -46,6 +54,15 @@ struct ContentView: View {
         }
         .edgesIgnoringSafeArea(.all)
         .padding(0)
+    }
+}
+
+struct TitleModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(.title, design: .serif))
+            .foregroundColor(Color("ColorGreenAdaptive"))
+            .padding(8)
     }
 }
 
